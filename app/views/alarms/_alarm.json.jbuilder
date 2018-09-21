46532a10c0ab01d61dde00a08eb665cca0ec8ebd
Myla Baker <myla.baker@gmail.com>
2018-09-21 12:09:03 -0700

    fix jbuilders for correct association, add upvote creation spec

--- app/views/alarms/_alarm.json.jbuilder
+++ app/views/alarms/_alarm.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! alarm, :id, :message, :created_at, :updated_at
-json.vote_count alarm.upvotes.present? ? alarm.upvotes.first.vote_count : 0
+json.vote_count alarm.upvote.present? ? alarm.upvote.vote_count : 0
 json.url alarm_url(alarm, format: :json)
---
+++ spec/controllers/upvotes_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe UpvotesController, type: :request do
+  let!(:alarm_1) { FactoryBot.create(:alarm) }
+  let!(:alarm_2) { FactoryBot.create(:alarm) }
+
+describe "POST upvote" do
+
+    it "Creates an upvote for an alarm and sets vote count to 1" do
+
+      post "/alarms/#{alarm_1.id}/upvotes.json"
+
+      res = JSON.parse(response.body)
+
+      expect(res["message"]).to eq alarm_1.message.upcase
+      expect(res["vote_count"]).to eq 1
+    end
+
+    it "Creates an upvote for an alarm and sets vote count to 1" do
+
+      post "/alarms/#{alarm_2.id}/upvotes.json"
+
+      post "/alarms/#{alarm_2.id}/upvotes.json"
+
+      res = JSON.parse(response.body)
+
+      expect(res["message"]).to eq alarm_2.message.upcase
+      expect(res["vote_count"]).to eq 2
+    end
+  end
+end
