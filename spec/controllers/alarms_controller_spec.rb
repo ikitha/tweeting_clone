b3ceb77ab28546b7cee2985e68db4e6eaf41fdae
Myla Baker <myla.baker@gmail.com>
2018-09-21 11:39:31 -0700

    add post endpoint test

--- spec/controllers/alarms_controller_spec.rb
+++ spec/controllers/alarms_controller_spec.rb
@@ -4,6 +4,9 @@ require 'spec_helper'
 RSpec.describe AlarmsController, type: :request do
   let!(:alarm_1) { FactoryBot.create(:alarm) }
   let!(:alarm_2) { FactoryBot.create(:alarm) }
+  let(:alarm_params) {
+     { message: "Sweet!" }
+  }
 
   describe "GET index" do
 
@@ -15,6 +18,18 @@ RSpec.describe AlarmsController, type: :request do
 
       expect(res.count).to eq 2
       expect(res[0]["created_at"] > res[1]["created_at"]).to be true
+    end
+  end
+
+  describe "POST index" do
+
+    it "Creates an alarm" do
+
+      post "/alarms.json", params: { alarm: alarm_params }
+
+      res = JSON.parse(response.body)
+
+      expect(res["message"]).to eq "SWEET!"
     end
   end
 end
